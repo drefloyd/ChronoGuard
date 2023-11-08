ce2f8d8951fb37b014db53fa9041d7b193569b40
drefloyd <64279881+drefloyd@users.noreply.github.com>
2023-11-08 11:14:25 -0500

    Move with WASD, look with mouse

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera Camera;
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+    private float ySensitivity = 30f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        // Calculates camera rotation
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        Camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // roatate player to look left/right
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+}
