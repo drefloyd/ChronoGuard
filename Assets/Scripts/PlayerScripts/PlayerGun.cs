de39115f386b3651c3d182250b88c5156171ccec
MohZaatari <91998180+MohZaatari@users.noreply.github.com>
2023-11-12 20:48:17 -0500

    fixed shooting, added reticle

--- Assets/Scripts/PlayerScripts/PlayerGun.cs
+++ Assets/Scripts/PlayerScripts/PlayerGun.cs
@@ -6,11 +6,21 @@ public class PlayerGun : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public float bulletSpeed = 13f;
+    public float bulletSpeed = 5000f;//make sure to change it on the player too, if ypu want to modify speed.
+    public Camera PlayerCam;
 
     public void shootBullet()
     {
+        RaycastHit theHit;
+        if(Physics.Raycast(PlayerCam.transform.position,PlayerCam.transform.forward,out theHit, Mathf.Infinity))
+        {
+            bulletSpawnPoint.rotation = Quaternion.LookRotation(theHit.point - bulletSpawnPoint.position); 
+        }
+        else
+        {
+            bulletSpawnPoint.rotation.Set(0, 0, 0, 0);
+        }
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnPoint.forward * bulletSpeed);
     }
 }
